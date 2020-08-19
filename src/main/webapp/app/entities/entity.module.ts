20427e086bf93b5591799f4d347fd35a5252408b
Arnal <aarrnnaall@gmail.com>
2020-08-18 22:13:35 -0300

    back_entity_next

--- src/main/webapp/app/entities/entity.module.ts
+++ src/main/webapp/app/entities/entity.module.ts
@@ -4,10 +4,6 @@ import { RouterModule } from '@angular/router';
 @NgModule({
   imports: [
     RouterModule.forChild([
-      {
-        path: 'friends',
-        loadChildren: () => import('./friends/friends.module').then(m => m.UmBookFriendsModule),
-      },
       /* jhipster-needle-add-entity-route - JHipster will add entity modules routes here */
     ]),
   ],
